0b9513fb6f054bc68d61e884b767b67ee82912a2
TheCraftySlayer <aaronnewe@gmail.com>
2024-07-02 10:38:26 -0600

    Update script_utility_functions.js

--- script_utility_functions.js
+++ script_utility_functions.js
@@ -1,154 +1,43 @@
+let directionsService;
+let directionsRenderer;
+
 function optimizeRoute() {
-    const waypoints = coordinatesList.slice(1, -1).map(coord => ({
-        location: coord,
-        stopover: true
-    }));
-
-    console.log('Waypoints:', waypoints); // Debug: Output waypoints
-
-    directionsService.route(
-        {
-            origin: coordinatesList[0],
-            destination: coordinatesList[coordinatesList.length - 1],
-            waypoints: waypoints,
-            optimizeWaypoints: true,
-            travelMode: google.maps.TravelMode.DRIVING
-        },
-        (response, status) => {
-            console.log("Directions response:", response);
-            if (status === 'OK') {
-                if (response && response.routes && response.routes.length > 0) {
-                    const route = response.routes[0];
-                    if (route && route.waypoint_order) {
-                        console.log("Optimized route:", route);
-
-                        const waypointOrder = route.waypoint_order;
-                        optimizedWaypoints = [coordinatesList[0], ...waypointOrder.map(i => coordinatesList[i + 1]), coordinatesList[coordinatesList.length - 1]];
-
-                        generateFullRoute();
-                    } else {
-                        console.error('No waypoint_order found in route:', route);
-                        alert('No waypoint_order found. Please check the coordinates and try again.');
-                    }
-                } else {
-                    console.error('No routes found in response:', response);
-                    alert('No routes found. Please check the coordinates and try again.');
-                }
-            } else {
-                console.error('Directions request failed due to:', status);
-                alert('Directions request failed due to ' + status);
-                handleDirectionsError(status);
-            }
-        }
-    );
-}
-
-function generateFullRoute() {
-    const waypoints = optimizedWaypoints.slice(1, -1).map(coord => ({
-        location: coord,
-        stopover: true
-    }));
-
-    directionsService.route(
-        {
-            origin: optimizedWaypoints[0],
-            destination: optimizedWaypoints[optimizedWaypoints.length - 1],
-            waypoints: waypoints,
-            travelMode: google.maps.TravelMode.DRIVING
-        },
-        (response, status) => {
-            console.log("Full route response:", response);
-            if (status === 'OK') {
-                directionsRenderer.setDirections(response);
-                
-                const route = response.routes[0];
-                const statsPanel = document.getElementById('routeStats');
-                const summaryPanel = document.getElementById('routeLinks');
-                const totalDistanceMiles = (route.legs.reduce((acc, leg) => acc + leg.distance.value, 0) / 1609.34).toFixed(2);
-                const totalDurationHours = (route.legs.reduce((acc, leg) => acc + leg.duration.value, 0) / 3600).toFixed(2);
-                statsPanel.innerHTML = `<b>Total Distance:</b> ${totalDistanceMiles} miles<br>`;
-                statsPanel.innerHTML += `<b>Total Duration:</b> ${totalDurationHours} hours<br>`;
-
-                const fullRouteLink = createFullRouteLink(optimizedWaypoints);
-                summaryPanel.innerHTML = `<a href="${fullRouteLink}" target="_blank">Full Route Link</a><br><br>`;
-                
-                document.getElementById('resetRoute').disabled = false;
-                document.getElementById('exportRoutes').disabled = false;
-                document.getElementById('sendEmail').disabled = false; // Enable the Send Email button
-            } else {
-                console.error('Full route request failed due to:', status);
-                alert('Full route request failed due to ' + status);
-                handleDirectionsError(status);
-            }
-        }
-    );
-}
-
-function createFullRouteLink(waypoints) {
-    const origin = waypoints[0];
-    const destination = waypoints[waypoints.length - 1];
-    const waypointsStr = waypoints.slice(1, -1).map(wp => `${wp.lat},${wp.lng}`).join('|');
-    return `https://www.google.com/maps/dir/?api=1&origin=${origin.lat},${origin.lng}&destination=${destination.lat},${destination.lng}&waypoints=${waypointsStr}&travelmode=driving`;
-}
-
-function handleDirectionsError(status) {
-    switch (status) {
-        case 'NOT_FOUND':
-            alert('One of the locations specified in the request\'s origin, destination, or waypoints could not be geocoded.');
-            break;
-        case 'ZERO_RESULTS':
-            alert('No route could be found between the origin and destination.');
-            break;
-        case 'MAX_WAYPOINTS_EXCEEDED':
-            alert('Too many waypoints were provided in the request.');
-            break;
-        case 'INVALID_REQUEST':
-            alert('The provided request was invalid.');
-            break;
-        case 'OVER_QUERY_LIMIT':
-            alert('The service has received too many requests from your application within the allowed time period.');
-            break;
-        case 'REQUEST_DENIED':
-            alert('The service denied the use of the Directions service by your application.');
-            break;
-        case 'UNKNOWN_ERROR':
-            alert('A directions request could not be processed due to a server error. Please try again.');
-            break;
-        default:
-            alert('An unknown error occurred.');
-            break;
+    if (!directionsService || !directionsRenderer) {
+        alert('Google Maps API is not fully initialized.');
+        return;
     }
-}
 
-function saveCurrentRoute(origin, destination, distance, duration, link) {
-    const routeDetails = {
-        origin: `${origin.lat()},${origin.lng()}`,
-        destination: `${destination.lat()},${destination.lng()}`,
-        distance: distance,
-        duration: duration,
-        link: link
+    const waypoints = coordinatesList.slice(1, -1).map(coord => ({
+        location: new google.maps.LatLng(coord.lat, coord.lng),
+        stopover: true
+    }));
+
+    const request = {
+        origin: coordinatesList[0],
+        destination: coordinatesList[coordinatesList.length - 1],
+        waypoints: waypoints,
+        optimizeWaypoints: true,
+        travelMode: google.maps.TravelMode.DRIVING
     };
-    savedRoutes.push(routeDetails);
-}
 
-function createSegmentRouteLink(origin, destination) {
-    const originStr = `${origin.lat()},${origin.lng()}`;
-    const destinationStr = `${destination.lat()},${destination.lng()}`;
-    return `https://www.google.com/maps/dir/?api=1&origin=${originStr}&destination=${destinationStr}`;
-}
+    directionsService.route(request, (response, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(response);
 
-function exportRoutesToCSV(routes) {
-    const headers = ['Origin', 'Destination', 'Distance', 'Duration', 'Link'];
-    const csvContent = [
-        headers.join(','),
-        ...routes.map(route => [route.origin, route.destination, route.distance, route.duration, route.link].join(','))
-    ].join('\n');
+            const route = response.routes[0];
+            const summaryPanel = document.getElementById('routeLinks');
+            summaryPanel.innerHTML = '';
 
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'routes.csv';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+            for (let i = 0; i < route.legs.length; i++) {
+                const routeSegment = i + 1;
+                summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+                summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            }
+        } else {
+            console.error('Directions request failed due to ' + status);
+            alert('Failed to optimize the route. Please try again.');
+        }
+    });
 }
