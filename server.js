5a9a10ff42a951d420cd9d917fd7a98a72585bd5
TheCraftySlayer <aaronnewe@gmail.com>
2024-07-02 16:52:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    tls: {
+        rejectUnauthorized: false,
+        servername: 'smtp.gmail.com'
+    },
+    family: 4 // Use IPv4
 });
 
 app.post('/send-email', (req, res) => {
