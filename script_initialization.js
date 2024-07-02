1e1a3b8aa736904eb937a94cf7c105c93e9ab7b3
TheCraftySlayer <aaronnewe@gmail.com>
2024-07-02 10:51:47 -0600

    Update script_initialization.js

--- script_initialization.js
+++ script_initialization.js
@@ -13,13 +13,6 @@ let upcData = [];
 let previousUPCs = [];
 let enteredUPCs = new Set();
 
-function initMap() {
-    // Initialization code moved here
-    directionsService = new google.maps.DirectionsService();
-    directionsRenderer = new google.maps.DirectionsRenderer();
-    directionsRenderer.setMap(map);
-}
-
 document.querySelectorAll('.garage-button').forEach(button => {
     button.addEventListener('click', function() {
         const lat = parseFloat(button.getAttribute('data-lat'));
